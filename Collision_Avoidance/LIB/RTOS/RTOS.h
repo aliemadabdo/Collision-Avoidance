92e559aba6a8dff65778a58df5666847d91ab598
Amr Ramadan <78983720+AmrRamadan01@users.noreply.github.com>
2023-09-07 00:23:14 +0300

    add RTOS error return type to RTOS.h

--- Collision_Avoidance/LIB/RTOS/RTOS.h
+++ Collision_Avoidance/LIB/RTOS/RTOS.h
@@ -15,24 +15,25 @@
 #include "../STD_Types.h"
 /* DEPENECIES END */
 
-#define TICK_TIME 20
-#define MAX_NUMBER_OF_TASKS 5
+#define RTOS_TICK_TIME 100
+#define RTOS_MAX_NUMBER_OF_TASKS 5
 
 typedef enum {
     TASK_OK, 
     TASK_CREATION_ERROR,
-    } error_type; 
+    } RTOS_error_t; 
 
 typedef struct {
     char name[];
     u8 ID;
     u8 prority;
     u8 pirodicity;
-    void (*service_routine) (void);
+    u8 wait_countdown;
+    void (*service_routine_ptr) (void);
 } RTOS_Task;
 
-void RTOS_create_task(char copy_name[], u8 copy_priority, u8 copy_piriodicty, void (*copy_service_routine)(void));
+RTOS_error_t RTOS_create_task(char copy_name[], u8 copy_priority, u8 copy_piriodicty, void (*copy_service_routine_ptr)(void));
 void RTOS_schedule(void);
 void RTOS_set_priority(char copy_name[], u8 copy_prioirty);
-
+void
 #endif
