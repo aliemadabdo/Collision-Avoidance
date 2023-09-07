5af36ea910716fe58b12213b0e85504ed8ebff2c
Ali Emad <90727865+aliemadabdo@users.noreply.github.com>
2023-09-07 19:15:57 +0300

    Done Discussion

--- Collision_Avoidance/Debug/main.d
+++ Collision_Avoidance/Debug/main.d
@@ -1,8 +1,11 @@
-main.o main.o: ../main.c ../LIB/STD_Types.h ../HAL/LCD/LCD.h \
-  ../HAL/DCMotor/DCMotor.h ../HAL/Ultrasonic/Ultrasonic.h \
-  ../LIB/RTOS/RTOS.h ../LIB/RTOS/../STD_Types.h
+main.o main.o: ../main.c ../LIB/STD_Types.h ../LIB/BIT_Math.h \
+  ../HAL/LCD/LCD.h ../HAL/DCMotor/DCMotor.h \
+  ../HAL/Ultrasonic/Ultrasonic.h ../LIB/RTOS/RTOS.h \
+  ../LIB/RTOS/../STD_Types.h
 
 ../LIB/STD_Types.h:
+
+../LIB/BIT_Math.h:
 
 ../HAL/LCD/LCD.h:
 
