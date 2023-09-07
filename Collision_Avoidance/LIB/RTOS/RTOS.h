cd65f5fd5bf1ac65a15432b95626c031b62fe0bb
Amr Ramadan <78983720+AmrRamadan01@users.noreply.github.com>
2023-09-07 14:32:23 +0300

    fix: add RTOS_MAX_TASK_NAME_LEN

--- Collision_Avoidance/LIB/RTOS/RTOS.h
+++ Collision_Avoidance/LIB/RTOS/RTOS.h
@@ -17,6 +17,7 @@
 
 #define RTOS_TICK_TIME 100
 #define RTOS_MAX_NUMBER_OF_TASKS 5
+#define RTOS_MAX_TASK_NAME_LEN 30
 
 typedef enum {
     TASK_OK, 
@@ -24,9 +25,9 @@ typedef enum {
     } RTOS_error_t; 
 
 typedef struct {
-    char name[];
+    char name[RTOS_MAX_TASK_NAME_LEN];
     u8 ID;
-    u8 prority;
+    u8 priority;
     u8 pirodicity;
     u8 wait_countdown;
     void (*service_routine_ptr) (void);
@@ -34,7 +35,7 @@ typedef struct {
 
 RTOS_error_t RTOS_create_task(char copy_name[], u8 copy_priority, u8 copy_piriodicty, void (*copy_service_routine_ptr)(void));
 void RTOS_schedule(void);
-void RTOS_sort_tasks_array(void);
+void RTOS_bubble_sort_task(u8 copy_start_index);
 void RTOS_set_priority(char copy_name[], u8 copy_prioirty);
 // sort RTOS_tasks_array() bassed on priority 
 #endif
