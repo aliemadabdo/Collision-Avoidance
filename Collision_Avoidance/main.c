d6206bc41d0659124d1b7568536a4b81682e8aa7
Amr Ramadan <78983720+AmrRamadan01@users.noreply.github.com>
2023-09-07 14:37:24 +0300

    test: create RTOS tasks for verification

--- Collision_Avoidance/main.c
+++ Collision_Avoidance/main.c
@@ -2,10 +2,23 @@
  * main.c
  *
  *  Created on: Sep 6, 2023
- *      Author: Ali Emad
+ *      Author: Ali Emad | Nada Atia | Amr Ramadan
  */
 #include <util/delay.h>
 #include "HAL/DCMotor/DCMotor.h"
+#include "LIB/RTOS/RTOS.h"
+
+
+void LED0_ISR (void){
+	// BlInk LED WITH PERiod = 10 us
+}
+void LED1_ISR (void){
+	// BlInk LED WITH PERiod = 500 us
+}
+void LED2_ISR (void){
+	// BlInk LED WITH PERiod = 50 ms
+}
+
 
 /* you can pass any value between 0 and 100 as a speed to the functions*/
 
@@ -13,6 +26,24 @@ int main(void){
 	int reading = 49 ;
 
 	DCMotor_voidInit();
+
+	RTOS_create_task(
+		"LED0",
+		0,                 //PRIORITY
+		1,				   //PIRODICITY
+		&LED0_ISR);
+
+	RTOS_create_task(
+		"LED1",
+		1,                 //PRIORITY
+		2,                 //PIRODICITY
+		&LED1_ISR);
+
+	RTOS_create_task(
+		"LED2",
+		2,                //PRIORITY
+		4,                //PIRODICITY
+		&LED2_ISR);
 
 	while(1){
 
